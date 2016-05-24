654ac8dc491075f9c126a7eb5bf4cde0b306fa17
Benjamin Dada <Benjamin Dada>
2016-05-24 01:11:13 +0100

    change the accordion to a panel collapsible

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
-  <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
 
     <!-- Custom styles for this template -->
     <link href="{{ asset('/css/main.css') }}" rel="stylesheet">
@@ -31,142 +31,144 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
 
 <body data-spy="scroll" data-offset="0" data-target="#navigation">
 
-<!-- Fixed navbar -->
-<div id="navigation" class="navbar navbar-default navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="{{url('/')}}">
-                WeHustleNG
-            </a>
+    <!-- Fixed navbar -->
+    <div id="navigation" class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="{{url('/')}}">
+                    WeHustleNG
+                </a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active"><a href="#home" class="smoothScroll">Home</a></li>
+                    <li><a href="#desc" class="smoothScroll">About</a></li>
+                    <li><a href="#features" class="smoothScroll">Do I Qualify?</a></li>
+                    <li><a href="#showcase" class="smoothScroll">Testimonials</a></li>
+                    <li><a href="#contact" class="smoothScroll">Contact</a></li>
+                </ul>
+            </div><!--/.nav-collapse -->
         </div>
-        <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="#home" class="smoothScroll">Home</a></li>
-                <li><a href="#desc" class="smoothScroll">About</a></li>
-                <li><a href="#features" class="smoothScroll">Do I Qualify?</a></li>
-                <li><a href="#showcase" class="smoothScroll">Testimonials</a></li>
-                <li><a href="#contact" class="smoothScroll">Contact</a></li>
-           </ul>
-        </div><!--/.nav-collapse -->
     </div>
-</div>
 
 
-<section id="home" name="home"></section>
-<img src="{{asset('img/wehustleng.jpg')}}" class="img-responsive" width="100%">
+    <section id="home" name="home"></section>
+    <img src="{{asset('img/wehustleng.jpg')}}" class="img-responsive" width="100%">
 
-<section id="desc" name="desc"></section>
-<!-- INTRO WRAP -->
-<div id="intro">
-    <div class="container">
-        <div class="row centered">
-            <h1>Member Groups</h1>
-            <br>
-            <br>
-            <div class="col-lg-4">
-                <img src="{{asset('img/about.png')}}" alt="">
-                <h3>Junior Hustlers</h3>
-                <p>Hustlers trying to get ahead in their business pursuits and want to learn from Successful entrepreneurs that are senior hustlers..</p>
-                <p>Usually they are new comers into the entrepreneurial race.</p>
-            </div>
-            <div class="col-lg-4">
-                <img src="{{asset('img/about.png')}}" alt="">
-                <h3>Senior Hustlers</h3>
-                <p>Hustlers with experience; they have been successful in their business pursuits and want to give back by mentoring aspiring entrepreneurs.</p>
-                <p>Usually they are the more successful and established entrepreneurs.</p>
-            </div>
-            <div class="col-lg-4">
-                <img src="{{asset('img/about.png')}}" alt="">
-                <h3>LOC Members</h3>
-                <p>As the name implies, they are the people that work behind the scenes to make the connections happen.</p>
-            </div>
-        </div>
-        <br>
-        <hr>
-    </div> <!--/ .container -->
-</div><!--/ #introwrap -->
-
-<!-- FEATURES WRAP -->
-<div id="features">
-    <div class="container">
-        <div class="row">
-            <h1 class="centered">Do I qualify?</h1>
-            <br>
-            <br>
-            <div class="col-lg-6 centered">
-                <img class="centered" src="{{asset('img/details.png')}}" alt="">
-            </div>
-
-            <div class="col-lg-6">
-                <h3>Some Criteria</h3>
+    <section id="desc" name="desc"></section>
+    <!-- INTRO WRAP -->
+    <div id="intro">
+        <div class="container">
+            <div class="row centered">
+                <h1>Member Groups</h1>
                 <br>
-                <!-- ACCORDION -->
-                <div class="accordion ac" id="accordion2">
-                    <div class="accordion-group">
-                        <div class="accordion-heading">
-                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
-                                Personality Culture
-                            </a>
-                        </div><!-- /accordion-heading -->
-                        <div id="collapseOne" class="accordion-body collapse in">
-                            <div class="accordion-inner">
-                                <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                            </div><!-- /accordion-inner -->
-                        </div><!-- /collapse -->
-                    </div><!-- /accordion-group -->
-                    <br>
-
-                    <div class="accordion-group">
-                        <div class="accordion-heading">
-                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo">
-                                Business Revenue
-                            </a>
-                        </div>
-                        <div id="collapseTwo" class="accordion-body collapse">
-                            <div class="accordion-inner">
-                                <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                            </div><!-- /accordion-inner -->
-                        </div><!-- /collapse -->
-                    </div><!-- /accordion-group -->
-                    <br>
-
-                    <div class="accordion-group">
-                        <div class="accordion-heading">
-                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseThree">
-                                Asset Declaration
-                            </a>
-                        </div>
-                        <div id="collapseThree" class="accordion-body collapse">
-                            <div class="accordion-inner">
-                                <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                            </div><!-- /accordion-inner -->
-                        </div><!-- /collapse -->
-                    </div><!-- /accordion-group -->
-                    <br>
-
-                    <div class="accordion-group">
-                        <div class="accordion-heading">
-                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseFour">
-                                Achievements
-                            </a>
-                        </div>
-                        <div id="collapseFour" class="accordion-body collapse">
-                            <div class="accordion-inner">
-                                <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                            </div><!-- /accordion-inner -->
-                        </div><!-- /collapse -->
-                    </div><!-- /accordion-group -->
-                    <br>
-                </div><!-- Accordion -->
+                <br>
+                <div class="col-lg-4">
+                    <img src="{{asset('img/about.png')}}" alt="">
+                    <h3>Junior Hustlers</h3>
+                    <p>Hustlers trying to get ahead in their business pursuits and want to learn from Successful entrepreneurs that are senior hustlers..</p>
+                    <p>Usually they are new comers into the entrepreneurial race.</p>
+                </div>
+                <div class="col-lg-4">
+                    <img src="{{asset('img/about.png')}}" alt="">
+                    <h3>Senior Hustlers</h3>
+                    <p>Hustlers with experience; they have been successful in their business pursuits and want to give back by mentoring aspiring entrepreneurs.</p>
+                    <p>Usually they are the more successful and established entrepreneurs.</p>
+                </div>
+                <div class="col-lg-4">
+                    <img src="{{asset('img/about.png')}}" alt="">
+                    <h3>LOC Members</h3>
+                    <p>As the name implies, they are the people that work behind the scenes to make the connections happen.</p>
+                </div>
             </div>
-        </div>
-    </div><!--/ .container -->
-</div><!--/ #features -->
+            <br>
+            <hr>
+        </div> <!--/ .container -->
+    </div><!--/ #introwrap -->
+
+    <!-- FEATURES WRAP -->
+    <div id="features">
+        <div class="container">
+            <div class="row">
+                <h1 class="centered">Do I qualify?</h1>
+                <br>
+                <br>
+                <div class="col-lg-2 centered">
+                    <img class="centered" src="{{asset('img/details.png')}}" alt="">
+                </div>
+
+                <div class="col-lg-8 col-lg-offset-2">
+                    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                    <div class="panel panel-default">
+                      <div class="panel-heading" role="tab" id="headingOne">
+                        <h4 class="panel-title">
+                          <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Age Bracket
+                          </a>
+                        </h4>
+                      </div>
+                      <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                        <div class="panel-body">
+                          Before we can extend an invitation to you must be between 15 and 35. <br>
+                          Say no to oldies!!! 
+                        </div>
+                      </div>
+                    </div>
+                    <div class="panel panel-default">
+                      <div class="panel-heading" role="tab" id="headingTwo">
+                        <h4 class="panel-title">
+                          <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Business Owner
+                          </a>
+                        </h4>
+                      </div>
+                      <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                        <div class="panel-body">
+                          For you to qualify as a hustler on our platform you must be either a founder or co-founder of a business.<br>
+                          Else, you would have to convince us on why 
+                        </div>
+                      </div>
+                    </div>
+                    <div class="panel panel-default">
+                      <div class="panel-heading" role="tab" id="headingThree">
+                        <h4 class="panel-title">
+                          <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Business Revenue
+                          </a>
+                        </h4>
+                      </div>
+                      <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                        <div class="panel-body">
+                          Company should have recorded a reasonable amount of sales from start date. <br>
+                          If you are not selling, what are you doing?
+                        </div>
+                      </div>
+                    </div>
+                    <div class="panel panel-default">
+                      <div class="panel-heading" role="tab" id="headingFour">
+                        <h4 class="panel-title">
+                          <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                            Customers
+                          </a>
+                        </h4>
+                      </div>
+                      <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
+                        <div class="panel-body">
+                          Your business should have paying customers.<br>
+                          Subscription basis could be a plus. 
+                        </div>
+                      </div>
+                    </div>
+                  </div><!--/ accordion -->
+                </div><!--/ lg-offset -->
+            </div><!--/ .row -->
+        </div><!--/ .container -->
+    </div><!--/ #features -->
 
 
 <section id="showcase" name="showcase"></section>
@@ -201,16 +203,15 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
     </div><!-- /container -->
 </div>
 
-
 <section id="contact" name="contact"></section>
-<section id="footerwrap">    
+<div id="footerwrap">    
     <a href="https://twitter.com/wehustleng">Tweet at Us!</a>
-</section>
+</div>
 </div>
 <div id="c">
     <div class="container">
         <p>
-           Copyright &copy; <?php echo date('Y'); ?> | Made with <i class="fa fa-heart"></i> by wehustleng. 
+         Copyright &copy; <?php echo date('Y'); ?> | Made with <i class="fa fa-heart-o "></i> by wehustleng 
         </p>
     </div>
 </div>
@@ -218,13 +219,13 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
 
 <!-- JavaScripts -->
 <!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<script>
-    $('.carousel').carousel({
-        interval: 3500
-    })
-</script>
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script>
+        $('.carousel').carousel({
+            interval: 3500
+        })
+    </script>
 </body>
 </html>
