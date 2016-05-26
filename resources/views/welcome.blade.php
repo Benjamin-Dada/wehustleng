6eabb56d149876b88033a12520007c4ab00c3108
Benjamin Dada <Benjamin Dada>
2016-05-26 13:40:42 +0100

    some more changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,8 +12,11 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
     <!-- Bootstrap core CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
 
+
+
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="{{asset('/css/scrolling-nav.css')}}">
 
     <!-- Custom styles for this template -->
     <link href="{{ asset('/css/main.css') }}" rel="stylesheet">
@@ -22,14 +25,14 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
     <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="{{ asset('/js/jquery.min.js') }}"></script>
-
+    <!-- <script src="{{ asset('/js/jquery.min.js') }}"></script>
+ -->
     <script src="{{ asset('/js/smoothscroll.js') }}"></script>
     
 
 </head>
 
-<body data-spy="scroll" data-offset="0" data-target="#navigation">
+<body id="page-top" data-spy="scroll" data-offset="0" data-target="#navigation">
 
     <!-- Fixed navbar -->
     <div id="navigation" class="navbar navbar-default navbar-fixed-top">
@@ -47,95 +50,86 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
             <div class="navbar-collapse collapse">
                 <ul class="nav navbar-nav navbar-right">
                     <li class="active"><a href="#home" class="smoothScroll">Home</a></li>
-                    <li><a href="#desc" class="smoothScroll">About</a></li>
+                    <li><a href="#desc" class="smoothScroll">Groups</a></li>
+                    <li><a href="#showcase" class="smoothScroll">Benefits</a></li>
                     <li><a href="#features" class="smoothScroll">Do I Qualify?</a></li>
                     <li><a href="#contact" class="smoothScroll">Contact</a></li>
-                    <li><a href="#showcase" class="smoothScroll">Testimonials</a></li>
+                    
                     
                 </ul>
             </div><!--/.nav-collapse -->
         </div>
     </div>
 
-
     <section id="home" name="home"></section>
-    <!-- <img src="{{asset('img/wehustleng.jpg')}}" class="img-responsive" width="100%"> -->
-
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
-    <li data-target="#myCarousel" data-slide-to="3"></li>
-  </ol>
-
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-      <img src="{{asset('img/wehustleng.jpg')}}" alt="Chania" style="width: 100%;">
-    </div>
-
-    <div class="item">
-      <img src="{{asset('img/wehustleng.jpg')}}" alt="Chania" style="width: 100%;">
-    </div>
-
-    <div class="item">
-      <img src="{{asset('img/wehustleng.jpg')}}" alt="Flower" style="width: 100%;">
-    </div>
-
-    <div class="item">
-      <img src="{{asset('img/wehustleng.jpg')}}" alt="Flower" style="width: 100%;">
-    </div>
-  </div>
-
-  <!-- Left and right controls -->
-  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div><!-- /carousel -->
+        <div id="myCarousel" class="carousel slide">
+          <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+          </ol>
+          <!-- Carousel items -->
+          <div class="carousel-inner">
+            <div class="active item"><img src="{{asset('img/wehustleng_plain.jpg')}}" alt="Chania" style="width: 100%;"></div>
+            <div class="item"><img src="{{asset('img/wehustleng_wit_def.jpg')}}" alt="Chania" style="width: 100%;"></div>
+            <div class="item"><img src="{{asset('img/wehustleng_wit_handle.jpg')}}" alt="Chania" style="width: 100%;"></div>
+          </div>
+          <!-- Carousel nav -->
+          <a class="carousel-control left" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+          <a class="carousel-control right" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+        </div>
 
     <section id="desc" name="desc"></section>
     <!-- INTRO WRAP -->
     <div id="intro">
         <div class="container">
             <div class="row centered">
-                <h1>Member Groups</h1>
+                <h2><b>Member Groups</b></h2>
                 <br>
                 <br>
                 <div class="col-lg-4">
-                    <i class="fa fa-users fa-5" aria-hidden="true"></i>
+                    <i class="fa fa-users fa-5x" aria-hidden="true"></i>
                     <h3>Junior Hustlers</h3>
-                    <p>Hustlers trying to get ahead in their business pursuits and want to learn from Successful entrepreneurs that are senior hustlers..</p>
-                    <p>Usually they are new comers into the entrepreneurial race.</p>
+                    <p>Young entrepreneurs, working towards breaking new grounds in their industry. Usually they have a working product/solution with seed capital/funding and what they need right now is distribution and PR/branding. With mentoring as an icing on the cake.</p>
                 </div>
                 <div class="col-lg-4">
-                    <i class="fa fa-users fa-5" aria-hidden="true"></i>
+                    <i class="fa fa-users fa-5x" aria-hidden="true"></i>
                     <h3>Senior Hustlers</h3>
-                    <p>Hustlers with experience; they have been successful in their business pursuits and want to give back by mentoring aspiring entrepreneurs.</p>
-                    <p>Usually they are the more successful and established entrepreneurs.</p>
+                    <p>Successful entrepreneurs with measurable experience building businesses. Typically they've either sold a business before or gone public or they could currently be running a business with 10 million naira in revenue.</p>
                 </div>
                 <div class="col-lg-4">
-                    <i class="fa fa-users fa-5" aria-hidden="true"></i>
-                    <h3>LOC Members</h3>
-                    <p>As the name implies, they are the people that work behind the scenes to make the connections happen.</p>
+                    <i class="fa fa-users fa-5x" aria-hidden="true"></i>
+                    <h3>Community Managers</h3>
+                    <p>As the name implies, they are the people that work behind the scenes to make the connections happen. Once they identify a fit between business needs they make the calls.<br>
+                    <a href="#" class="btn btn-default">Volunteer now</a>
+                    </p>
                 </div>
             </div>
             <br>
             <hr>
         </div> <!--/ .container -->
     </div><!--/ #introwrap -->
-
+    <section id="showcase" name="showcase"></section>
+    <div id="showcase">
+        <div class="container">
+            
+                <h2 class="centered"><b>Benefits</b></h2>
+                <br>
+                <div class="col-lg-8 col-lg-offset-2">
+                    
+                </div>
+            
+            <br>
+            <br>
+            <br>
+        </div><!-- /container -->
+    </div>
     <!-- FEATURES WRAP -->
     <div id="features">
         <div class="container">
             <div class="row">
-                <h1 class="centered">Do I qualify?</h1>
+                <h2 class="centered"><b>Do I qualify?</b></h2>
+                <br>
                 
                 <div class="col-lg-2 centered">
                     <i class="fa fa-list-alt fa-5"></i>
@@ -153,7 +147,7 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
                       </div>
                       <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
                         <div class="panel-body">
-                          Before we can extend an invitation to you must be between 15 and 35. <br>
+                          Before we can extend an invitation to you must be between ages 15 and 40. <br>
                           Say no to oldies!!! 
                         </div>
                       </div>
@@ -192,14 +186,15 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
                       <div class="panel-heading" role="tab" id="headingFour">
                         <h4 class="panel-title">
                           <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                            Customers
+                            Business Financing
                           </a>
                         </h4>
                       </div>
                       <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
                         <div class="panel-body">
-                          Your business should have paying customers.<br>
-                          Subscription basis could be a plus. 
+                          Your business should have some level of financing already.<br>
+                          Typically, this platform is not set for hungry people who are desperately in need of funding.<br>
+                          It can so happen that 2 people in the same community have a need of starting a business and the other investing in a business. <br> Our Community Managers can make that happen to.
                         </div>
                       </div>
                     </div>
@@ -215,44 +210,14 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
     <a href="mailto:wehustleng@gmail.com">Email us, now!</a>
 </div>
 
-<section id="showcase" name="showcase"></section>
-<div id="showcase">
-    <div class="container">
-        <div class="row">
-            <h1 class="centered">Testimonials</h1>
-            <br>
-            <div class="col-lg-8 col-lg-offset-2">
-                <div id="carousel-example-generic" class="carousel slide">
-                    <!-- Indicators -->
-                    <ol class="carousel-indicators">
-                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                    </ol>
 
-                    <!-- Wrapper for slides -->
-                    <div class="carousel-inner">
-                        <div class="item active">
-                            <img src="{{asset('img/wehustleng_plain.jpg')}}" alt="">
-                        </div>
-                        <div class="item">
-                            <img src="{{asset('img/wehustleng_plain.jpg')}}" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <br>
-        <br>
-        <br>
-    </div><!-- /container -->
-</div>
 
 <div id="c">
     <div class="container" style="padding-bottom: 10px;">
-        <p style="display: inline;">
+        <p >
          Copyright &copy; <?php echo date('Y'); ?> | Made with <a href="#"><i class="fa fa-heart-o "></i></a> by wehustleng 
         </p>
-        <div class="pull-right"> <a href="https://wehustleng.herokuapp.com/"> <i class="fa fa-envelope-o"></i> &nbsp;Tweet at us! </a></div>
+        <p> <a href="https://twitter.com/wehustleng"> <i class="fa fa-twitter"></i> &nbsp;Tweet at us! </a></p>
     </div>
 </div>
 
@@ -262,10 +227,13 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script>
+    <!-- <script>
         $('.carousel').carousel({
             interval: 3500
         })
-    </script>
+    </script> -->
+    <!-- Scrolling Nav JavaScript -->
+    <script src="{{asset('js/jquery.easing.min.js')}}"></script>
+    <script src="{{asset('js/scrolling-nav.js')}}"></script>
 </body>
 </html>
